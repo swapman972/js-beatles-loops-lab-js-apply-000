b4d653c40d9b2fc706e88da910ebbf7b08551da8
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-02-26 21:31:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(array1, array2){
   var array = []
   for (let i= 0; i <array.length; i++){
-    rray.push(array1[i]+ " plays " + array2[i])
+    array.push(array1[i]+ " plays " + array2[i])
   }
   return array
 }
