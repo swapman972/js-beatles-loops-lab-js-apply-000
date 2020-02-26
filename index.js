d24890fb09c1b49148250d260679ba48a3e0d225
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-02-26 21:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay(array1, array2){
+  var array = []
+  for (let i= 0; i <array.length; i++){
+    var string = array1[i]+ " plays " + array2[i]
+    array.push(string)
+  }
+  return array
+}
+
